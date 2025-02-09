b7a1fa357a6fa65146a8d89e7d332d15abb5c219
SungJungHo1 <109999302+SungJungHo1@users.noreply.github.com>
2025-02-09 16:24:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ thread_Count = 0
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-client = MongoClient('mongodb://admin2:asd64026@13.209.74.215', 27017)
+client = MongoClient(
+    "mongodb://admin:your_password@13.209.74.215:27017/?authSource=admin"
+)
 db = client.KoreaServer
 collection  = db.users
 
